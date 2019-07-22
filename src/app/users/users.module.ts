ba266eb6491baf05d0d39df0b0fe9eca0b090165
Jhonattan Flores <jdevys2012@gmail.com>
2019-07-21 21:49:27 -0400

    Added user-card component to render user's data

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserListComponent } from './components/user-list/user-list.component';
+import { UserCardComponent } from './components/user-card/user-card.component';
 
 
 
 @NgModule({
-  declarations: [UserListComponent],
+  declarations: [UserListComponent, UserCardComponent],
   imports: [
     CommonModule
   ]
